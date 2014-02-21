82ce8d149049564d8cdbcf592d4ef21e97396209
Joao Gabriel <joao.2507@gmail.com>
2014-02-21 15:05:55 -0300

    - load config json

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ function exitFromAccount(buttonIndex) {
 }
 
 function onClickToPlayer() {
-    //alert(devicePlataform);
+    alert(devicePlataform);
     alert(config_streaming.url.Android);
 //    if(devicePlataform == 'Android')
 //        window.open(config_streaming.url.Android, '_system');
